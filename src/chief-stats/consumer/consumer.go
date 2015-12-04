00192c515a6271f661de23e2375bdca1d1415a34
Sergei O. Udalov <sergei.udalov@gmail.com>
2015-12-04 13:58:38 +0300

    cid, sid

--- src/chief-stats/consumer/consumer.go
+++ src/chief-stats/consumer/consumer.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/sha256"
+	"fmt"
 	"github.com/jrallison/go-workers"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
+
+const Year = 365 * 24 * time.Hour
 
 func main() {
 	workers.Configure(map[string]string{
@@ -21,6 +27,37 @@ func main() {
 	})
 
 	http.HandleFunc("/api/p/v", func(w http.ResponseWriter, r *http.Request) {
+		//===========================================================================
+		// Client id
+		client_id_cookie, _ := r.Cookie("cid")
+
+		if client_id_cookie == nil {
+			client_id_cookie = &http.Cookie{Name: "cid", Value: randomID()}
+		}
+
+		client_id_cookie.Expires = time.Now().Add(3 * Year)
+
+		w.Header().Add("Set-Cookie", client_id_cookie.String())
+
+		//===========================================================================
+		// Client id
+
+		session_id_cookie, _ := r.Cookie("sid")
+
+		if session_id_cookie == nil {
+			session_id_cookie = &http.Cookie{Name: "sid", Value: randomID()}
+		}
+
+		session_id_cookie.Expires = time.Now().Add(30 * time.Minute)
+
+		w.Header().Add("Set-Cookie", session_id_cookie.String())
+
+		//===========================================================================
+
+		// Add a job to a queue
+		// client_id, session_id, ip, user_agent, url, referer
+		go workers.Enqueue("pageview", "Add", []string{client_id_cookie.Value, session_id_cookie.Value})
+		log.Printf("cid: %s sid: %s", client_id_cookie.Value, session_id_cookie.Value)
 
 		pic, err := os.Open("./public/blank_1px.gif")
 
@@ -31,8 +68,15 @@ func main() {
 		io.Copy(w, pic)
 	})
 
-	go workers.StatsServer(8081)
-	go workers.Run()
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
+func randomID() (id string) {
+	data := make([]byte, 10)
+
+	if _, err := rand.Read(data); err == nil {
+		id = fmt.Sprintf("%x", sha256.Sum256(data))
+	}
+
+	return id
 }
