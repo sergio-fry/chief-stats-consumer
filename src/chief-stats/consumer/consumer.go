a3bcc0a7ceeb42b8bb67d31873df1a0178f5c001
Sergei O. Udalov <sergei.udalov@gmail.com>
2015-12-05 16:55:53 +0300

    works

--- src/chief-stats/consumer/consumer.go
+++ src/chief-stats/consumer/consumer.go
@@ -28,43 +28,39 @@ func main() {
 	})
 
 	http.HandleFunc("/api/p/v", func(w http.ResponseWriter, r *http.Request) {
-		//===========================================================================
-		// Client id
-		client_id_cookie, _ := r.Cookie("cid")
-
-		if client_id_cookie == nil {
-			client_id_cookie = &http.Cookie{Name: "cid", Value: randomID()}
-		}
-
-		client_id_cookie.Expires = time.Now().Add(3 * Year)
-
+		client_id_cookie := clientCookie(r)
 		w.Header().Add("Set-Cookie", client_id_cookie.String())
 
-		//===========================================================================
-		// Client id
-
-		session_id_cookie, _ := r.Cookie("sid")
-
-		if session_id_cookie == nil {
-			session_id_cookie = &http.Cookie{Name: "sid", Value: randomID()}
-		}
-
-		session_id_cookie.Expires = time.Now().Add(30 * time.Minute)
-
+		session_id_cookie := sessionCookie(r)
 		w.Header().Add("Set-Cookie", session_id_cookie.String())
 
-		//===========================================================================
+		var job_params []interface{}
+
+		job_params = append(job_params, "PageViewConsumerJob")
+		job_params = append(job_params, []string{
+			client_id_cookie.Value,
+			session_id_cookie.Value,
+			r.Header.Get("X-Real-IP"),
+			r.UserAgent(),
+			r.FormValue("url"),
+			r.FormValue("referer"),
+		})
 
 		// Add a job to a queue
 		// client_id, session_id, ip, user_agent, url, referer
-		go workers.Enqueue("pageview", "Add", []string{client_id_cookie.Value, session_id_cookie.Value})
-		log.Printf("cid: %s sid: %s", client_id_cookie.Value, session_id_cookie.Value)
+		go workers.Enqueue("pageview", "JobRunner", job_params)
+
+		log.Print(job_params)
 
 		pic, err := os.Open("./public/blank_1px.gif")
 
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		w.Header().Add("Cache-Control", "private, no-cache, no-store, must-revalidate, max-age=0")
+		w.Header().Add("Pragma", "no-cache")
+		w.Header().Add("Expires", "0")
 
 		io.Copy(w, pic)
 	})
@@ -80,4 +76,27 @@ func randomID() (id string) {
 	}
 
 	return id
+}
+
+func clientCookie(r *http.Request) *http.Cookie {
+	cookie, _ := r.Cookie("cid")
+
+	if cookie == nil {
+		cookie = &http.Cookie{Name: "cid", Value: randomID()}
+	}
+	cookie.Expires = time.Now().Add(3 * Year)
+
+	return cookie
+}
+
+func sessionCookie(r *http.Request) *http.Cookie {
+	cookie, _ := r.Cookie("sid")
+
+	if cookie == nil {
+		cookie = &http.Cookie{Name: "sid", Value: randomID()}
+	}
+
+	cookie.Expires = time.Now().Add(30 * time.Minute)
+
+	return cookie
 }
